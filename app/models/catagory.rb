6c802d3e04babe72e2ea944a3ea90e4e49449eb7
Maha Amjad <apple@Apples-MacBook-Pro.local>
2020-09-10 19:33:22 +0500

    first commit

--- app/models/catagory.rb
+++ app/models/catagory.rb
@@ -1,6 +1,5 @@
 class Catagory < ApplicationRecord
-  #  belongs_to :car
-  #  belogns_to :motorcycle
+  
     scope :cars, -> { where(type: 'Car') } 
   scope :motorcycles, -> { where(type: 'Motorcycle') }
 end
